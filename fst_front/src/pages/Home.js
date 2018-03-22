2552471883d34233e68becebb8b92cc5ae1f96e5
davidmgudeman <davidmgudeman@gmail.com>
2018-03-22 09:52:28 -0700

    Added Auth2 which is an antd form. The form shows up without much styling.

--- fst_front/src/pages/Home.js
+++ fst_front/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Link, Route } from 'react-router-dom';
 import Transfer from './Transfer';
 import Auth from './Auth';
+import { WrappedHorizontalLoginForm }  from './Auth2';
 import { transfer2 } from './transfer2';
 import { test } from './test';
 
@@ -16,11 +17,13 @@ class Home extends Component {
                         <li><Link to='/transfer'>Transfer</Link></li>
                         <li><Link to='/test'>test</Link></li>
                         <li><Link to='/transfer2'>Transfer2</Link></li>
+                        <li><Link to='auth2'>Auth2</Link></li>
                     </ul>
                     <Route exact path="/auth" component={Auth}/>
                     <Route path="/transfer" component={Transfer}/>
                     <Route path="/test" component={test}/>
                     <Route path="/transfer2" component={transfer2}/>
+                    <Route path={"/auth2"} component={WrappedHorizontalLoginForm}/>
 
                 </nav>
             </div>
