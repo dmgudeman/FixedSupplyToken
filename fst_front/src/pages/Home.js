bd8917f544f8b0435479a7bb52090be00949ce4e
davidmgudeman <davidmgudeman@gmail.com>
2018-03-22 10:17:24 -0700

    added Auth3 which is a better approximation of what we need

--- fst_front/src/pages/Home.js
+++ fst_front/src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link, Route } from 'react-router-dom';
 import Transfer from './Transfer';
 import Auth from './Auth';
 import { WrappedHorizontalLoginForm }  from './Auth2';
+import { WrappedDynamicRule } from './Auth3'
 import { transfer2 } from './transfer2';
 import { test } from './test';
 
@@ -17,13 +18,15 @@ class Home extends Component {
                         <li><Link to='/transfer'>Transfer</Link></li>
                         <li><Link to='/test'>test</Link></li>
                         <li><Link to='/transfer2'>Transfer2</Link></li>
-                        <li><Link to='auth2'>Auth2</Link></li>
+                        <li><Link to='/auth2'>Auth2</Link></li>
+                        <li><Link to='/auth3'>Auth3</Link></li>
                     </ul>
                     <Route exact path="/auth" component={Auth}/>
                     <Route path="/transfer" component={Transfer}/>
                     <Route path="/test" component={test}/>
                     <Route path="/transfer2" component={transfer2}/>
                     <Route path={"/auth2"} component={WrappedHorizontalLoginForm}/>
+                    <Route path={"/auth3"} component={WrappedDynamicRule}/>
 
                 </nav>
             </div>
